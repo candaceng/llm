402890e5b82813dd1128bdba8bae09bbd18fd42e
candaceng <candacewyng@gmail.com>
2024-09-16 14:44:57 -0400

    restructure + batching

--- llama.py
+++ llama.py
@@ -1,101 +1,83 @@
 import pickle
-from dotenv import load_dotenv
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 import transformers
 import json
 from collections import Counter
 from torch import cuda, bfloat16
+from helper_functions import *
 
-load_dotenv()
-
-def adjust_probabilities(original_probs, sample_prompts):
+def adjust_probabilities(original_probs, prompts):
     """ Adjusts output probabilities based on relative frequency and marginal probabilities
     
     :param original_probs: tensor of probabilities from one inference prompt
-    :param sample_prompts: list of sample prompts to calculate marginal probabilities
+    :param prompts: list of sample prompts to calculate marginal probabilities
 
     :return: new output probabilities
     """
     # Tokenize the sample prompts
-    tokenizer.pad_token = tokenizer.eos_token
-    inputs = tokenizer(sample_prompts, return_tensors="pt", padding=True).to(model.device)
+    inputs = tokenizer(prompts, return_tensors="pt", padding=True, truncation=True).to(model.device)
 
     # Generate logits from the model and convert to probabilities
     with torch.no_grad():
         logits = model(**inputs).logits
-        vocab_size = logits.shape[-1]
-    probs = torch.softmax(logits, dim=-1)
+        probs = torch.softmax(logits, dim=-1)
 
     # Calculate marginal probabilities 
     token_prob_sum = probs.sum(dim=(0, 1))
-    total_positions = probs.shape[0] * probs.shape[1]
-    marginal_probs = token_prob_sum / total_positions 
+    marginal_probs = token_prob_sum / probs.numel()
 
     # Obtain frequencies for words in the model tokenizer
-    frequency_tensor = torch.ones(vocab_size) * 1.0/total_tokens
-    for token_id in range(vocab_size):
-        token = tokenizer.convert_ids_to_tokens(token_id).lstrip('▁')
-        if token in frequency_dict:
-            frequency_tensor[token_id] = frequency_dict[token]
-    frequency_tensor = frequency_tensor.unsqueeze(0).unsqueeze(0).to(model.device)
+    vocab_size = logits.shape[-1]
+    token_ids = torch.arange(vocab_size, device=model.device)
+    tokens = tokenizer.convert_ids_to_tokens(token_ids.tolist())
+    
+    # Set frequencies based on the frequency_dict
+    frequency_tensor = torch.ones(vocab_size, device=model.device) * (1.0 / total_tokens)
+    token_freqs = [frequency_dict.get(token.lstrip('▁'), 1.0 / total_tokens) for token in tokens]
+    frequency_tensor = torch.tensor(token_freqs, device=model.device).unsqueeze(0).unsqueeze(0)
 
     adjusted_probs = original_probs * frequency_tensor / marginal_probs
 
     return adjusted_probs
 
 
-def get_new_prediction(prompt, target):
+def get_new_prediction(prompts):
     """ 
-    :param prompt: the prompt to run inference on
-    :param target: torch tensor of the encoded next word token of the prompt
+    :param prompts: batch of prompts to run inference on
 
-    :return: next predicted word after adjusting output probabilities
+    :return: [new next word predictions, new probabilities]
     """
 
-    inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+    inputs = tokenizer(prompts, return_tensors="pt", padding=True).to(model.device)
 
     with torch.no_grad():
         logits = model(**inputs).logits
     
     original_probs = torch.softmax(logits, dim=-1)
-    adjusted_probs = adjust_probabilities(original_probs, sample_prompts)
+    adjusted_probs = adjust_probabilities(original_probs, prompts)
 
-    pred = tokenizer.decode(torch.argmax(adjusted_probs, dim=-1)[0][-1])
-    original_loss = get_prob_loss(original_probs, target)
-    adjusted_loss = get_prob_loss(adjusted_probs, target)
+    preds_ids = torch.argmax(adjusted_probs, dim=-1)
+    preds = [tokenizer.decode(pred_id) for pred_id in preds_ids[:, -1]]
+    
+    return preds, adjusted_probs
 
-    return pred, original_loss, adjusted_loss
+def get_loss(probs, target, loss='accuracy'):
+    logits = torch.log(probs)[:, -1, :] 
+
+    if loss == 'cross_entropy':
+        target = torch.tensor(target, dtype=torch.long).unsqueeze(0).to(model.device)
+        result = torch.nn.functional.cross_entropy(logits.view(-1, logits.size(-1)), target).item()
+    elif loss == 'accuracy':
+        predicted_token_id = torch.argmax(logits, dim=-1).item()
+        result = int(predicted_token_id == target)
+
+    return result
 
 
-def get_prob_loss(probs, target):
-
-    # get last row of predictions (next word after full prompt)
-    logits = torch.log(probs[:, -1, :]) 
-    loss = torch.nn.functional.cross_entropy(logits.view(-1, logits.size(-1)), target)
-
-    return loss
-
-def get_accuracies():
-    pass
-
-def initialize_model(model_name="meta-llama/Llama-2-7b-hf"):
-    """Creates tokenizer and model if downloaded files are not available"""
-
-    access_token = os.getenv('HF_ACCESS_TOKEN')
-    tokenizer = AutoTokenizer.from_pretrained(model_name, token=access_token)
-
-    model = AutoModelForCausalLM.from_pretrained(
-        model_name,
-        device_map={"": 0},
-        quantization_config=bnb_config,
-        token=access_token
-    )
-    model.config.use_cache = False
-    model.config.pretraining_tp = 1
-
-    return tokenizer, model
-
+tokenizer = AutoTokenizer.from_pretrained('./tokenizer_files')
+tokenizer.pad_token = tokenizer.eos_token
+model = AutoModelForCausalLM.from_pretrained('./model_files')
 
 with open('token_counts.json', 'r', encoding='utf-8') as f:
     frequency_dict = json.load(f)
@@ -105,6 +87,7 @@ with open('token_counts.json', 'r', encoding='utf-8') as f:
 with open("prompts_and_labels.pkl", "rb") as fp:  
     prompts_and_labels = pickle.load(fp)
     sample_prompts = [row[0] for row in prompts_and_labels]
+    sample_labels = [tokenizer.decode(torch.tensor([row[1]], dtype=torch.long)) for row in prompts_and_labels]
 
 bnb_config = transformers.BitsAndBytesConfig(
     load_in_4bit=True,
@@ -112,9 +95,6 @@ bnb_config = transformers.BitsAndBytesConfig(
     bnb_4bit_use_double_quant=True,
     bnb_4bit_compute_dtype=bfloat16
 )
-
-tokenizer = AutoTokenizer.from_pretrained('./tokenizer_files')
-model = AutoModelForCausalLM.from_pretrained('./model_files')
 
 pipeline = transformers.pipeline(
     "text-generation",
@@ -124,8 +104,11 @@ pipeline = transformers.pipeline(
     device_map="auto",
 )
 
-prompt = 'hey what is going'
-# print(f'Original prediction: {pipeline(prompt, max_new_tokens=1)[0]["generated_text"]}')
-# print(f'New prediction: {prompt} {get_new_prediction(prompt)}')
+# original_preds = [res[0]['generated_text'].split()[-1] for res in pipeline(sample_prompts[:3], max_new_tokens=1)]
 
-print(get_new_prediction(prompt, torch.tensor([373], dtype=torch.long).to(model.device)))
+prompt = 'the banana is'
+# print(f'Original prediction: {pipeline(prompt, max_new_tokens=1)[0]["generated_text"]}')
+# print(f'New prediction: {prompt} {get_new_prediction(prompt)[0]}')
+
+prompts = ['i want to', 'hey what is going']
+print(get_new_prediction(prompts)[0])
